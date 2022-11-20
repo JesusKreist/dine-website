02b0e2c5101ccb1277f9f611cd7a61a66207687e
Samuel Ude <59575776+JesusKreist@users.noreply.github.com>
2022-11-20 09:36:19 +0100

    Move intro hero section to separate file and fix bg colour

---
+++ src/components/HomePage/IntroSection.tsx
@@ -0,0 +1,50 @@
+import { Box, Button, Heading, Text, VStack, HStack } from "@chakra-ui/react";
+import Logo from "../logo/Logo";
+
+const IntroSection = () => {
+  return (
+    <Box
+      bgImage={`linear-gradient(to right, black, black 39%, rgba(158, 127, 102, 0.01) 39%), url(/homepage/hero-bg-desktop@2x.jpg)`}
+      bgRepeat="no-repeat"
+      paddingBottom="6rem"
+      bgSize="contain"
+      height="820px"
+    >
+      <Box
+        paddingTop="4rem"
+        paddingLeft="10rem"
+        paddingBottom="6rem"
+        width="50%"
+        color="white"
+      >
+        <Logo />
+        <Heading
+          as="h2"
+          fontSize={"80px"}
+          fontWeight="light"
+          lineHeight={"80px"}
+          marginTop="9rem"
+          paddingBottom="0.5rem"
+        >
+          Exquisite dining since 1989
+        </Heading>
+        <Text fontSize="xl" width="70%" paddingBottom="2rem">
+          Experience our seasonal menu in beautiful country surroundings. Eat
+          the freshest produce from the comfort of our farmhouse.
+        </Text>
+        <Button
+          borderRadius="none"
+          variant="outline"
+          padding="2rem 3rem"
+          fontSize="xl"
+          fontWeight="bold"
+          letterSpacing="0.2ch"
+        >
+          BOOK A TABLE
+        </Button>
+      </Box>
+    </Box>
+  );
+};
+
+export default IntroSection;
