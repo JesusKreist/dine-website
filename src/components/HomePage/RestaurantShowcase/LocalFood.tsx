de0b8e53ae43e93ff0222e73c19a8b9427d75aa8
Samuel Ude <59575776+JesusKreist@users.noreply.github.com>
2022-12-20 08:33:58 +0100

    Add tablet style for LocalFood component

--- src/components/HomePage/RestaurantShowcase/LocalFood.tsx
+++ src/components/HomePage/RestaurantShowcase/LocalFood.tsx
@@ -5,41 +5,65 @@ import RestaurantShowcaseText from "./RestaurantShowcaseText";
 const LocalFood = () => {
   return (
     <Fragment>
-      <RestaurantShowcaseText
-        heading="The most locally sourced food"
-        text="All our ingredients come directly from our farm or local fishery. So you can be sure that you're eating the freshest, most sustainable food."
-        withDivider
-      />
+      <Box display={{ md: "none", lg: "block" }}>
+        <RestaurantShowcaseText
+          heading="The most locally sourced food"
+          text="All our ingredients come directly from our farm or local fishery. So you can be sure that you're eating the freshest, most sustainable food."
+          withDivider
+        />
+      </Box>
 
-      <Box height="720px">
+      <Box
+        position="relative"
+        height={{ md: "360px", lg: "720px" }}
+        width={{ md: "573px", lg: "unset" }}
+        margin={{ md: "0 auto", lg: "unset" }}
+      >
         <Image
+          srcSet="/homepage/locally-sourced-tablet@2x.jpg 768w, /homepage/locally-sourced-desktop@2x.jpg 1440w"
+          sizes="(max-width: 1200px) 768px, 1440px"
           src="/homepage/locally-sourced-desktop@2x.jpg"
           alt="image showing locally sourced food"
           objectFit="contain"
           height="100%"
-          marginLeft="-4rem"
+          marginLeft={{ lg: "-4rem" }}
         />
         <Box
-          width="895px"
-          marginLeft="-16rem"
           position="relative"
-          bottom="400px"
+          bottom={{ md: "-290px", lg: "unset" }}
+          className="second_pic_box"
+          left={{ md: "530px", lg: "unset" }}
           zIndex="-1"
+          width={{ lg: "895px" }}
+          marginLeft={{ md: "-14rem", lg: "-16rem" }}
+          height={{ md: "320px", lg: "unset" }}
         >
           <Image
             src="patterns/pattern-curve-top-left.svg"
             alt="curve pattern"
-            objectFit="contain"
+            objectFit={{ md: "cover", lg: "contain" }}
+            objectPosition={{ md: "left", lg: "unset" }}
+            position="relative"
+            bottom={{ md: "130px", lg: "400px" }}
+            height="100%"
+            zIndex={-1}
           />
         </Box>
         <Box
           width="160px"
           position="relative"
-          bottom="760px"
-          marginLeft="27rem"
+          bottom={{ md: "450px", lg: "760px" }}
+          marginLeft={{ md: "29rem", lg: "27rem" }}
         >
           <Image src="patterns/pattern-lines.svg" alt="lines pattern" />
         </Box>
+      </Box>
+      <Box display={{ md: "block", lg: "none" }} zIndex="2">
+        <RestaurantShowcaseText
+          heading="The most locally sourced food"
+          text="All our ingredients come directly from our farm or local fishery. So you can be sure that you're eating the freshest, most sustainable food."
+          withDivider
+        />
       </Box>
     </Fragment>
   );
