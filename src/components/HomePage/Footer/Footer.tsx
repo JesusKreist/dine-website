c49aa45733cddd8dea4a0e2f4eb27df778d9b578
Samuel Ude <59575776+JesusKreist@users.noreply.github.com>
2022-12-21 19:28:53 +0100

    Add tablet style to Footer component

--- src/components/HomePage/Footer/Footer.tsx
+++ src/components/HomePage/Footer/Footer.tsx
@@ -4,25 +4,45 @@ import Logo from "../../logo/Logo";
 
 const Footer = () => {
   return (
-    <Box height="240px" bgColor="black" paddingLeft="10rem" color="white">
+    <Box
+      display={{ md: "flex", lg: "block" }}
+      height={{ md: "328px", lg: "240px" }}
+      bgColor="black"
+      paddingLeft={{ lg: "10rem" }}
+      color="white"
+      alignItems="center"
+      justifyContent="center"
+    >
       <Flex
         alignItems="center"
-        height="100%"
-        columnGap="12rem"
-        fontSize="17px"
-        letterSpacing=".25ch"
+        height="200px"
+        gap={{ md: "9rem", lg: "12rem" }}
+        fontSize={{ md: "14px", lg: "17px" }}
+        lineHeight={{ md: "28px", lg: "28px" }}
+        letterSpacing="2px"
       >
-        <Logo />
-        <Box width="244px" height="84px">
-          <Text>MARTHWAITE, SEDBERGH</Text>
-          <Text>CUMBRIA</Text>
-          <Text>+00 44 123 4567</Text>
+        <Box alignSelf={{ md: "start", lg: "unset" }}>
+          <Logo />
         </Box>
-        <Box width="446px" height="84px">
-          <Text>OPEN TIMES</Text>
-          <Text>MON - FRI: 09:00 AM - 10:00 PM</Text>
-          <Text>SAT - SUN: 09:00 AM - 11:30 PM</Text>
-        </Box>
+
+        <Flex
+          gap={{ lg: "12rem" }}
+          width={{ md: "456px", lg: "unset" }}
+          height={{ md: "200px", lg: "unset" }}
+          direction={{ md: "column", lg: "row" }}
+          justifyContent={{ md: "space-between", lg: "unset" }}
+        >
+          <Box width={{ md: "100%", lg: "244px" }} height="84px">
+            <Text>MARTHWAITE, SEDBERGH</Text>
+            <Text>CUMBRIA</Text>
+            <Text>+00 44 123 4567</Text>
+          </Box>
+          <Box width={{ md: "100%", lg: "446px" }} height="84px">
+            <Text>OPEN TIMES</Text>
+            <Text>MON - FRI: 09:00 AM - 10:00 PM</Text>
+            <Text>SAT - SUN: 09:00 AM - 11:30 PM</Text>
+          </Box>
+        </Flex>
       </Flex>
     </Box>
   );
